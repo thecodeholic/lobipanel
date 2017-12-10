6b4978cf5d15f309a11d4d366c233e104b7f4e38
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2017-12-10 22:56:06 +0400

    Implement js/css minification

--- gulpfile.js
+++ gulpfile.js
@@ -4,38 +4,52 @@
 
 'use strict';
 
+var pjson = require('./package.json');
+
 var less = require('gulp-less');
 var path = require('path');
 var gulp = require('gulp');
 var copy = require('gulp-copy');
-// var uglify = require('gulp-uglify');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var cleanCSS = require('gulp-clean-css');
 // var pump = require('pump');
 
 
 gulp.task('less', function () {
   return gulp.src('./less/lobipanel.less')
     .pipe(less({
-      paths: [ path.join(__dirname, 'less', 'includes') ]
-    }))
-    .pipe(gulp.dest('./css'));
-});
-
-gulp.task('cssmin', function () {
-  return gulp.src('./less/lobipanel.less')
-    .pipe(less({
-      paths: [ path.join(__dirname, 'less', 'includes') ]
+      paths: [path.join(__dirname, 'less', 'includes')]
     }))
     .pipe(gulp.dest('./css'));
 });
 
 gulp.task('copy', function () {
+  gulp.src('./js/**/*.js')
+    .pipe(gulp.dest('./dist/js'));
+  gulp.src('./css/**/*.css')
+    .pipe(gulp.dest('./dist/css'));
 
-   gulp.src('./js/**/*.js')
-        .pipe(gulp.dest('./dist/js'));
+});
+
+gulp.task('cssmin', function () {
+  return gulp.src('./css/lobipanel.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('uglify', function () {
+  gulp.src([
+    './js/lobipanel.js'
+  ])
+    .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./dist/js'));
 
 });
 
 gulp.task('watch', function () {
-  gulp.watch('./less/**/*.less',['less']);
-  gulp.watch('./less/**/*.less',['sc']);
+  gulp.watch('./less/**/*.less', ['less']);
+  gulp.watch('./less/**/*.less', ['sc']);
 });
